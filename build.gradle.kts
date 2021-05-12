96d33586cb3a40fa2c311a32ea492dca5835f92a
Rebecca Schmidt <65017227+rebeccasc@users.noreply.github.com>
2021-05-12 15:45:50 +0200

    Add slf4j dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("junit:junit:4.13.1")
     implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    compile(group="org.slf4j", name="slf4j-api", version="1.7.25")
+    compile(group="org.slf4j", name="slf4j-simple", version="1.7.25")
     implementation ("org.opensourcebim:BuildingSMARTLibrary:1.0.9")
 }
 
