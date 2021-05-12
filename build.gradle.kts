57969ab2e793bf6de60c47639714b8a73a3d350b
Rebecca Schmidt <65017227+rebeccasc@users.noreply.github.com>
2021-05-12 22:10:34 +0200

    Update build.gradle.kts
    Add sourceSet, slf4j logger and JUnit5 dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,19 +12,51 @@ repositories {
     mavenCentral()
 }
 
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src/main/kotlin"))
+        }
+        resources {
+            setSrcDirs(listOf("src/main/resources"))
+        }
+    }
+    test {
+        java {
+            setSrcDirs(listOf("src/test/kotlin"))
+        }
+        resources {
+            setSrcDirs(listOf("src/test/resources"))
+        }
+    }
+}
+
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("junit:junit:4.13.1")
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    compile(group="org.slf4j", name="slf4j-api", version="1.7.25")
-    compile(group="org.slf4j", name="slf4j-simple", version="1.7.25")
+    api(group="org.slf4j", name="slf4j-api", version="1.7.25")
+    api(group="org.slf4j", name="slf4j-simple", version="1.7.25")
     implementation ("org.opensourcebim:BuildingSMARTLibrary:1.0.9")
+
+    // tests
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.2.0")
+}
+
+tasks {
+    // Use the native JUnit support of Gradle.
+    "test"(Test::class) {
+        useJUnitPlatform()
+    }
 }
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
