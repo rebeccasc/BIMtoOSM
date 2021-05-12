6d19a42be636e41973debdbd63942939c61eb7d1
Rebecca Schmidt <65017227+rebeccasc@users.noreply.github.com>
2021-05-12 14:36:21 +0200

    Add license text to source files

--- src/main/kotlin/de/rebsc/bimtoosm/parser/BIMtoOSMParser.kt
+++ src/main/kotlin/de/rebsc/bimtoosm/parser/BIMtoOSMParser.kt
@@ -1,5 +1,22 @@
 package de.rebsc.bimtoosm.parser
 
+/******************************************************************************
+ * Copyright (C) 2021  de.rebsc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ *****************************************************************************/
+
 import de.rebsc.bimtoosm.api.BIMtoOSM
 import de.rebsc.bimtoosm.data.OSMDataSet
 import de.rebsc.bimtoosm.exception.BIMtoOSMException
@@ -9,8 +26,8 @@ import java.nio.file.Paths
 
 class BIMtoOSMParser(private var config: Configuration) : BIMtoOSM {
 
-    private lateinit var file : File
-    private lateinit var ifcModel : ModelPopulation
+    private lateinit var file: File
+    private lateinit var ifcModel: ModelPopulation
 
     override fun configure(config: Configuration) {
         this.config = config
@@ -27,7 +44,7 @@ class BIMtoOSMParser(private var config: Configuration) : BIMtoOSM {
      * Loads ifc file into model
      * @throws BIMtoOSMException
      */
-    private fun loadFile(filepath: String){
+    private fun loadFile(filepath: String) {
         try {
             file = File(filepath)
             val inputFs = file.inputStream()
@@ -39,7 +56,7 @@ class BIMtoOSMParser(private var config: Configuration) : BIMtoOSM {
             ifcModel.load()
             inputFs.close()
 
-        } catch (e: NullPointerException){
+        } catch (e: NullPointerException) {
             throw BIMtoOSMException("Could not load $filepath")
         }
     }
--- src/test/kotlin/de/rebsc/bimtoosm/parser/BIMtoOSMParserTest.kt
+++ src/test/kotlin/de/rebsc/bimtoosm/parser/BIMtoOSMParserTest.kt
@@ -1,10 +1,26 @@
 package de.rebsc.bimtoosm.parser
 
+/******************************************************************************
+ * Copyright (C) 2021  de.rebsc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see {@literal<http://www.gnu.org/licenses/>}.
+ *****************************************************************************/
+
 import de.rebsc.bimtoosm.exception.BIMtoOSMException
 import de.rebsc.bimtoosm.geometry.GeometrySolution
 import org.junit.Assert.assertThrows
 import org.junit.Test
-
 
 internal class BIMtoOSMParserTest {
 
