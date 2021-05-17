61039770063c05ad531b0fd3c88641edd65e7e0e
Rebecca Schmidt <65017227+rebeccasc@users.noreply.github.com>
2021-05-17 16:55:15 +0200

    Implement PlacementResolver

--- src/main/kotlin/de/rebsc/bimtoosm/parser/BIMtoOSMParser.kt
+++ src/main/kotlin/de/rebsc/bimtoosm/parser/BIMtoOSMParser.kt
@@ -19,9 +19,22 @@ package de.rebsc.bimtoosm.parser
 
 import de.rebsc.bimtoosm.api.BIMtoOSM
 import de.rebsc.bimtoosm.data.OSMDataSet
+import de.rebsc.bimtoosm.geometry.PlacementResolver
 import de.rebsc.bimtoosm.loader.Loader
 import de.rebsc.bimtoosm.logger.Logger
 import de.rebsc.bimtoosm.optimizer.BIMFileOptimizer
+import org.bimserver.emf.IfcModelInterface
+import org.bimserver.emf.Schema
+import org.bimserver.models.ifc4.IfcWall as Ifc4_IfcWall
+import org.bimserver.models.ifc4.IfcSlab as Ifc4_IfcSlab
+import org.bimserver.models.ifc4.IfcColumn as Ifc4_IfcColumn
+import org.bimserver.models.ifc4.IfcDoor as Ifc4_IfcDoor
+import org.bimserver.models.ifc4.IfcStair as Ifc4_IfcStair
+import org.bimserver.models.ifc2x3tc1.IfcWall as Ifc2x3tc1_IfcWall
+import org.bimserver.models.ifc2x3tc1.IfcSlab as Ifc2x3tc1_IfcSlab
+import org.bimserver.models.ifc2x3tc1.IfcColumn as Ifc2x3tc1_IfcColumn
+import org.bimserver.models.ifc2x3tc1.IfcDoor as Ifc2x3tc1_IfcDoor
+import org.bimserver.models.ifc2x3tc1.IfcStair as Ifc2x3tc1_IfcStair
 import kotlin.properties.Delegates
 
 
@@ -55,11 +68,9 @@ class BIMtoOSMParser(config: Configuration) : BIMtoOSM {
     }
 
     override fun parse(filepath: String): OSMDataSet {
-        var ifcFilepath = filepath
-
         // pre-processing
         status = ParserStatus.PRE_PROCESSING
-        ifcFilepath = BIMFileOptimizer.optimizeIfcFile(
+        val ifcFilepath: String = BIMFileOptimizer.optimizeIfcFile(
             filepath,
             config.optimizeInput_RBC,
             config.optimizeInput_RBL
@@ -67,12 +78,12 @@ class BIMtoOSMParser(config: Configuration) : BIMtoOSM {
 
         // load into model and extract ifc environment vars
         status = ParserStatus.LOADING
-        val ifcModel = Loader.loadIntoModel(ifcFilepath)
-        val ifcUnitPrefix = PropertiesExtractor.extractIfcUnits(ifcModel)
+        val model = Loader.loadIntoModel(ifcFilepath)
+        val units = PropertiesExtractor.extractIfcUnits(model)
 
         // transform ifc to osm
         status = ParserStatus.PARSING
-        // TODO implement
+        val osmData = transformToOSM(model, units)
 
         // post-processing
         if (config.optimizeOutput_DS) {
@@ -80,11 +91,62 @@ class BIMtoOSMParser(config: Configuration) : BIMtoOSM {
             // TODO implement
         }
 
-        return OSMDataSet()
+        return osmData
     }
 
     override fun status(): String {
         return status.toString()
+    }
+
+    /**
+     * Transform each object in [model] into OSM object and put it into [OSMDataSet]
+     * @param model ifc model
+     * @param units ifc unit prefix
+     * @return [OSMDataSet]
+     */
+    private fun transformToOSM(model: IfcModelInterface, units: IfcUnitPrefix): OSMDataSet {
+        val schema = model.modelMetaData.ifcHeader.ifcSchemaVersion
+
+        // resolve placements
+        val placementResolver = PlacementResolver()
+        if (schema == Schema.IFC4.headerName) {
+            model.getAllWithSubTypes(Ifc4_IfcWall::class.java).forEach { wall ->
+                placementResolver.resolvePlacement(wall.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc4_IfcSlab::class.java).forEach { slab ->
+                placementResolver.resolvePlacement(slab.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc4_IfcColumn::class.java).forEach { column ->
+                placementResolver.resolvePlacement(column.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc4_IfcDoor::class.java).forEach { door ->
+                placementResolver.resolvePlacement(door.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc4_IfcStair::class.java).forEach { stair ->
+                placementResolver.resolvePlacement(stair.objectPlacement)
+            }
+        }
+        if (schema == Schema.IFC2X3TC1.headerName) {
+            model.getAllWithSubTypes(Ifc2x3tc1_IfcWall::class.java).forEach { wall ->
+                placementResolver.resolvePlacement(wall.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc2x3tc1_IfcSlab::class.java).forEach { slab ->
+                placementResolver.resolvePlacement(slab.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc2x3tc1_IfcColumn::class.java).forEach { column ->
+                placementResolver.resolvePlacement(column.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc2x3tc1_IfcDoor::class.java).forEach { door ->
+                placementResolver.resolvePlacement(door.objectPlacement)
+            }
+            model.getAllWithSubTypes(Ifc2x3tc1_IfcStair::class.java).forEach { stair ->
+                placementResolver.resolvePlacement(stair.objectPlacement)
+            }
+        }
+
+        // TODO resolve representation
+
+        return OSMDataSet()
     }
 
 }
