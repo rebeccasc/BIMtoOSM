bab79dfc764e5213b37471a2ed6d7cc1d0b7be6d
Rebecca Schmidt <65017227+rebeccasc@users.noreply.github.com>
2021-05-17 12:37:05 +0200

    Update LoaderTest

--- src/test/kotlin/de/rebsc/bimtoosm/parser/LoaderTest.kt
+++ src/test/kotlin/de/rebsc/bimtoosm/parser/LoaderTest.kt
@@ -19,6 +19,7 @@ package de.rebsc.bimtoosm.parser
 
 import de.rebsc.bimtoosm.loader.Loader
 import de.rebsc.bimtoosm.exception.BIMtoOSMException
+import org.bimserver.plugins.deserializers.DeserializeException
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -28,19 +29,34 @@ internal class LoaderTest {
     fun loadFileTest() {
         val dir = System.getProperty("user.dir")
 
+        //------------ test invalid file ------------ //
         // test non-existent ifc file
         Assertions.assertThrows(BIMtoOSMException::class.java) {
             val filepath = "$dir/src/test/resources/test1_IFC4_.ifc"
             Loader.loadIntoModel(filepath)
         }
 
-        // TODO test invalid ifc file
+        // Test invalid file with block comments
+        Assertions.assertThrows(DeserializeException::class.java) {
+            val filepath = "$dir/src/test/resources/test2_IFC2X3_TC1_BC.ifc"
+            Loader.loadIntoModel(filepath)
+        }
 
-        // test valid ifc file
+        // TODO test more invalid ifc file
+
+        //------------ test valid file ------------ //
+        // test valid IFC4 file
         Assertions.assertDoesNotThrow {
             val filepath = "$dir/src/test/resources/test1_IFC4.ifc"
             Loader.loadIntoModel(filepath)
         }
+
+        // test valid IFC2X3_TC1 file
+        Assertions.assertDoesNotThrow {
+            val filepath = "$dir/src/test/resources/test2_IFC2X3_TC1.ifc"
+            Loader.loadIntoModel(filepath)
+        }
+
     }
 
 }
